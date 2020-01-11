ca2d464afac01735a448e6a65024dc85967b87be
hzoou <woojooo52@naver.com>
2020-01-12 03:37:11 +0900

    feat: 타이틀 컴포넌트 구현
    타이틀 컴포넌트를 구현했습니다.

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styled from './style';
+
+const Title = () => {
+    return (
+        <Styled.Title>My Todo</Styled.Title>
+    )
+};
+
+export default Title
---
+++ src/components/Title/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Title = styled.header`
+    height: 10%;
+    font-size: 2.5rem;
+    font-weight: 900;
+    color: white;
+`;
+
+export { Title }
